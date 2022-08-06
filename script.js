104d76e63bc51672b3e20c853f332a987204b795
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-08-06 16:15:02 +0530

    some sentence comment out
     for testing

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.querySelector("#generate").addEventListener("click", () => {
 generate = () => {
   var myData = {
     //replicate the data make another 
-    "Random": "In the aftermath of my tooth extraction, I have returned home  ~ Hinglish meaning of this line  : mere daant nikaalane ke baad, main ghar laut aaya hoon ",
+    // "Random": "In the aftermath of my tooth extraction, I have returned home  ~ Hinglish meaning of this line  : mere daant nikaalane ke baad, main ghar laut aaya hoon ",
     // "Flexibility of the word FUCK 😂": "Sexual: Let's fuck.  Mischief: Let's fuck with them.  Aggression: Let's fuck them up.  Anger: Fuck you.    Regret: Fuck me!     Resignation: Oh, fuck it     Surprise: Holy fuck!    Puzzlement: What the fuck?     Greeting: How the fuck are you?      Fraud: I got fucked over.          Identification: Who the fuck are you?     Philosophical: Who gives a fuck?     Compliment: She's so fucking beautiful.    Confusion: Where the fuck are we?     Wonder: What the fuck is that?     Amazement: How the fuck did he do that?           Threat: Don't fuck with me!             Realization: I fucking knew it!",
     "Ig: The Knowledge Spreader": "Keep it down  ~ Hinglish meaning of this line  : Thoda kam shor Machao ",
     "Ig: The Knowledge Spreader": "Latch/bolt the door  ~ Hinglish meaning of this line  : Kundi laga do ",
@@ -16,7 +16,7 @@ generate = () => {
     "Ig: The Knowledge Spreader": "Breakfast  ~ Pronounce  : Break-fst ",
     "Ig: The Knowledge Spreader": "Dessert  ~ Pronounce  : Di-Zuhrt ",
     "Ig: The Knowledge Spreader": "Jewellery  ~ Pronounce  : Jool-Ri ",
-    "Ig: The Knowledge Spreader": "Tomorrow  ~ Pronounce  : To-Maw-ro ",
+    // "Ig: The Knowledge Spreader": "Tomorrow  ~ Pronounce  : To-Maw-ro ",
   };
 
   
