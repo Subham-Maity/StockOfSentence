24e1a4c83725c6567bb225bf8b1203af3b3b82f2
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-08-06 15:26:44 +0530

    Flexibility of the word "FUCK"

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ generate = () => {
   var myData = {
     //replicate the data make another 
     "Random": "In the aftermath of my tooth extraction, I have returned home  ~ Hinglish meaning of this line  : mere daant nikaalane ke baad, main ghar laut aaya hoon ",
-    "Movie - wanda": " Hello World!\nThis is my string",
+    "Flexibility of the word FUCK 😂": "Sexual: Let's fuck.  Mischief: Let's fuck with them.  Aggression: Let's fuck them up.  Anger: Fuck you.    Regret: Fuck me!     Resignation: Oh, fuck it     Surprise: Holy fuck!    Puzzlement: What the fuck?     Greeting: How the fuck are you?      Fraud: I got fucked over.          Identification: Who the fuck are you?     Philosophical: Who gives a fuck?     Compliment: She's so fucking beautiful.    Confusion: Where the fuck are we?     Wonder: What the fuck is that?     Amazement: How the fuck did he do that?           Threat: Don't fuck with me!             Realization: I fucking knew it!",
+
   };
 
   
