a4bf91511135be132af1743413d9c983dfa76e90
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-09-23 12:47:29 +0530

    I'll confront him

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ generate = () => {
     "10-Ocean English Academy": "Just a quick call to update you that today's meeting has been called off ~ Hinglish meaning of this line  : aapako apadet karane ke lie bas ek tvarit kol ki aaj kee baithak ko band kar diya gaya hai🙂",
     "10-Random ": "skeptical ~ an attitude of doubt 🤔",
     "11-Random ": "I just took a year off.The love of fitness never fades ~ mainne abhee ek saal kee chhuttee lee hai.fitness ka pyaar kabhee kam nahin hota 💪",
+    "12-Random ": " I'll confront him  ~ main usaka saamana karoonga 💪 ",
+
   };
 
   
