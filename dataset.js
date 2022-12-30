1bcf51ed8e7371ed0c120a4acba245377e763bea
ellamsg <ellamsgeorge7@gmail.com>
2022-12-30 02:16:29 -0800

    added Game of thrones quote

--- dataset.js
+++ dataset.js
@@ -1838,9 +1838,10 @@ generate = () => {
     "Hope is a good thing, maybe the best of things.😊 And no good thing ever dies.🌸 ~Meaning: Hope is a powerful emotion that can keep us going through difficult times. It is an enduring force that can sustain us even in the darkest of moments.💪",
     
 "67- The Godfather":
-    "I'm gonna make him an offer he can't refuse.🤑 ~Meaning: This line means that the speaker is going to present an offer that the other person will not be able to turn down, either because it is too good to pass up or because they will face negative consequences if they do not accept it.💰"
-      
-    
+    "I'm gonna make him an offer he can't refuse.🤑 ~Meaning: This line means that the speaker is going to present an offer that the other person will not be able to turn down, either because it is too good to pass up or because they will face negative consequences if they do not accept it.💰",
+
+"68- Game Of Thrones":
+     "Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you."
     
 
 
